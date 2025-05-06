6e3e005c4d44a9ffad88f52173688e162fc6f270
Samiksha Kute <samikshakute2002@gmail.com>
2025-05-06 11:11:00 +0530

    Add files via upload

---
+++ AdvancedWebAutomation/Miscelleanous.java
@@ -0,0 +1,21 @@
+package AdvancedWebAutomation;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Miscelleanous{
+    public static void main(String[] args) throws IOException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("https://www.google.com/");
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(src, new File("D:\\screenshot.png"));
+    }
+}
---
+++ AdvancedWebAutomation/SSLCheck.java
@@ -0,0 +1,17 @@
+package AdvancedWebAutomation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SSLCheck {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://expired.badssl.com/");
+        System.out.println(driver.getTitle());
+
+        // https://developer.chrome.com/docs/chromedriver/capabilities
+    }
+}
