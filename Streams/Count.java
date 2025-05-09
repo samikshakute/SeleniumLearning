f3e4b51f83a4ba4ca73a161af5503c3c01680495
Samiksha Kute <samikshakute2002@gmail.com>
2025-05-09 19:08:41 +0530

    add new code files

---
+++ Streams/Count.java
@@ -0,0 +1,16 @@
+package Streams;
+
+import java.util.List;
+
+public class Count {
+    public static void main(String[] args) {
+        List<String> names = List.of("Alexandra", "Don", "Alok", "Adam", "Rama");
+        int count = 0;
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                count++;
+            }
+        }
+        System.out.println("Count: " + count);
+    }
+}
---
+++ Streams/StreamCount.java
@@ -0,0 +1,17 @@
+package Streams;
+
+import java.util.List;
+
+public class StreamCount {
+    public static void main(String[] args) {
+        List<String> names = List.of("Alexandra", "Don", "Alok", "Adam", "Rama");
+
+        // Use streams to count names starting with "A"
+        long count = names.stream()
+                .filter(s -> s.startsWith("A"))
+                .count();
+
+        // Print the result
+        System.out.println("Count: " + count);
+    }
+}
